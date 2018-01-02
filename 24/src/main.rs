c3284e6d7b5506a12fbc6e1c008cd090c77b3e7b
Niedoba Dariusz <darek969-12@o2.pl>
2018-01-02 01:04:59 +0100

    Completed 24th day

--- 24/src/main.rs
+++ 24/src/main.rs
@@ -1,24 +1,54 @@
 fn main() {
     let components = read_components();
-    println!("1: {}", calculate_max_strength(0, components));
+    println!("1: {}", calculate_max_strength(0, &components));
+    println!("2: {:?}", calculate_max_length(0, &components));
 }
 
-fn calculate_max_strength(end: usize, components: Vec<Component>) -> usize {
+fn calculate_max_strength(end: usize, components: &[Component]) -> usize {
     let mut strength = 0;
-    for component in &components {
+    for component in components {
         if let Some(other_end) = component.get_other_end(end) {
-            let filtered = components
+            let filtered: Vec<_> = components
                 .iter()
                 .filter(|&x| *x != *component)
                 .map(|x| *x)
                 .collect();
 
             let current_strength = end + other_end +
-                calculate_max_strength(other_end, filtered);
+                calculate_max_strength(other_end, &filtered);
             strength = strength.max(current_strength);
         }
     }
     return strength;
+}
+
+fn calculate_max_length(end: usize, components: &[Component]) -> (usize, usize) {
+    let mut length = 0;
+    let mut strength = 0;
+    for component in components {
+        if let Some(other_end) = component.get_other_end(end) {
+            let filtered: Vec<_> = components
+                .iter()
+                .filter(|&x| *x != *component)
+                .map(|x| *x)
+                .collect();
+
+            let (l, s) = calculate_max_length(other_end, &filtered);
+            let current_length = 1 + l;
+            let current_strength = end + other_end + s;
+
+            if current_length > length {
+                length = current_length;
+                strength = current_strength;
+            } else if current_length == length {
+                if current_strength > strength {
+                    length = current_length;
+                    strength = current_strength;
+                }
+            }
+        }
+    }
+    return (length, strength);
 }
 
 fn read_components() -> Vec<Component> {
